14a22d0bc81e40cc4d09cf7fe31c86f009380b20
Robert Palmer <jfritz@robotniko.de>
2005-07-13 17:17:04 +0000

    Config Dialog etwas größer gemacht, da beim MAC nicht alles in das Fenster gepasst hat.

--- src/de/moonflower/jfritz/dialogs/config/ConfigDialog.java
+++ src/de/moonflower/jfritz/dialogs/config/ConfigDialog.java
@@ -54,6 +54,9 @@ import de.moonflower.jfritz.utils.Encryption;
 import de.moonflower.jfritz.utils.JFritzUtils;
 import de.moonflower.jfritz.utils.network.SSDPPacket;
 
+import java.net.URLEncoder;
+import java.net.URLDecoder;
+
 /**
  * JDialog for JFritz configuration.
  *
@@ -133,7 +136,7 @@ public class ConfigDialog extends JDialog {
 								"")));
 		passwordAfterStartButton.setSelected(pwAfterStart);
 
-		pass.setText(JFritzUtils.replaceSpecialCharsURL(Encryption.decrypt(JFritz.getProperty("box.password")), false));
+		pass.setText(URLEncoder.encode(Encryption.decrypt(JFritz.getProperty("box.password"))));
 		encodedPassword = Encryption.decrypt(JFritz.getProperty("box.password"));
 		address.setText(JFritz.getProperty("box.address"));
 		areaCode.setText(JFritz.getProperty("area.code"));
@@ -277,7 +280,7 @@ public class ConfigDialog extends JDialog {
 		ActionListener actionListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Object source = e.getSource();
-				encodedPassword = JFritzUtils.replaceSpecialCharsURL(new String(pass.getPassword()), true);
+				encodedPassword = URLDecoder.decode(new String(pass.getPassword()));
 				pressed_OK = (source == pass || source == okButton);
 				if (source == pass || source == okButton
 						|| source == cancelButton) {
@@ -523,7 +526,7 @@ public class ConfigDialog extends JDialog {
 
 		addKeyListener(keyListener);
 
-		setSize(new Dimension(480, 350));
+		setSize(new Dimension(480, 380));
 		setResizable(false);
 		// pack();
 	}
