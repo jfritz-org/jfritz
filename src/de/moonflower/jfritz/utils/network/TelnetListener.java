e71c6d5170081a8126aec79a70cb29b3faff83d2
Robert Palmer <jfritz@robotniko.de>
2005-07-16 11:48:52 +0000

    Umgeschrieben und Teile in die neue Klasse Telnet geschrieben

--- src/de/moonflower/jfritz/utils/network/TelnetListener.java
+++ src/de/moonflower/jfritz/utils/network/TelnetListener.java
@@ -1,16 +1,11 @@
 package de.moonflower.jfritz.utils.network;
 
-import org.apache.commons.net.telnet.*;
-
 import de.moonflower.jfritz.JFritz;
 import de.moonflower.jfritz.utils.Debug;
-import de.moonflower.jfritz.utils.Encryption;
+import de.moonflower.jfritz.utils.network.Telnet;
+import de.moonflower.jfritz.exceptions.WrongPasswordException;
+import java.io.IOException;
 
-import java.io.*;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -34,134 +29,33 @@ public class TelnetListener extends Thread {
 
 	private final String PATTERN_VOIP_CALLTO_DISCONNECTED = "disconnected\\([^)]*\\):";
 
-	private final char prompt = '#';
+	private Telnet telnet;
 
-	private TelnetClient telnet = new TelnetClient();
-
-	private InputStream in;
-
-	private PrintStream out;
+	public TelnetListener(JFritz jfritz) {
+		// Fetch new calls
+		jfritz.getJframe().getFetchButton().doClick();
+		telnet = new Telnet();
+		Debug.msg("Starting TelnetListener");
+		telnet.connect();
+		start();
+	}
 
 	public void run() {
 		Debug.msg("run()");
-		if (!telnet.isConnected()) {
-			connectTelnet();
-			restartTelefonDaemon();
-			parseOutput();
-		}
-	}
-
-	private void connectTelnet() {
-		String server = JFritz.getProperty("box.address");
-		String user = "";
-		String password = Encryption
-				.decrypt(JFritz.getProperty("box.password"));
-		int port = 23;
-		try {
-			telnet.connect(server, port); // Connect to the specified server
-			in = telnet.getInputStream();
-			out = new PrintStream(telnet.getOutputStream());
-			login(user, password);
-			readUntil(prompt + " ");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		restartTelefonDaemon();
+		parseOutput();
 	}
 
 	private void restartTelefonDaemon() {
-		write("killall telefon && telefon &>&1");
+		telnet.write("killall telefon && telefon &>&1");
 		Debug.msg("Telefon Daemon restarted.");
-	}
-
-	private void startSyslogDaemon() {
-		String ip = "192.168.178.20";
-		int port = 4711;
-		write("syslogd -R " + ip + ":" + port);
-		write("killall telefon && telefon | logger &");
-	}
-
-	/**
-	 * @param user
-	 * @param password
-	 */
-	private void login(String user, String password) {
-		if (!user.equals("")) {
-			// Log the user on
-			readUntil("ogin: ");
-			write(user);
-		}
-		if (!password.equals("")) {
-			readUntil("assword: ");
-			write(password);
-		}
-		Debug.msg("Logged into Telnet connection.");
-	}
-
-	public void su(String password) {
-		try {
-			write("su");
-			readUntil("Password: ");
-			write(password);
-			readUntil(prompt + " ");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public String readUntil(String pattern) {
-		try {
-			char lastChar = pattern.charAt(pattern.length() - 1);
-			StringBuffer sb = new StringBuffer();
-			boolean found = false;
-			char ch = (char) in.read();
-			while (!isInterrupted()) {
-				sb.append(ch); // FIXME This can be done better!!!
-				if (ch == lastChar) {
-					if (sb.toString().endsWith(pattern)) {
-						return sb.toString();
-					}
-				}
-				ch = (char) in.read();
-			}
-		} catch (Exception e) {
-			Debug.err(e.getMessage());
-			interrupt();
-		}
-		return null;
-	}
-
-	public void write(String value) {
-		try {
-			out.println(value);
-			out.flush();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public String sendCommand(String command) {
-		try {
-			write(command);
-			return readUntil(prompt + " ");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public void disconnect() {
-		Debug.msg("Disconnect Telnet connection.");
-		try {
-			telnet.disconnect();
-		} catch (Exception e) {
-		}
 	}
 
 	public void parseOutput() {
 		try {
 			String currentLine = "";
 			while (!isInterrupted()) {
-				currentLine = readUntil("\n");
+				currentLine = telnet.readUntil("\n");
 				if (isInterrupted())
 					break;
 				Pattern p = Pattern.compile(PATTERN_TELEFON);
@@ -196,28 +90,11 @@ public class TelnetListener extends Thread {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		disconnect();
+		telnet.disconnect();
 	}
 
-	public static InetAddress getIP() {
-		Enumeration ifaces;
-		try {
-			ifaces = NetworkInterface.getNetworkInterfaces();
-			while (ifaces.hasMoreElements()) {
-				NetworkInterface ni = (NetworkInterface) ifaces.nextElement();
-				System.out.println(ni.getName() + ":");
-
-				Enumeration addrs = ni.getInetAddresses();
-
-				while (addrs.hasMoreElements()) {
-					InetAddress addr = (InetAddress) addrs.nextElement();
-					System.out.println(" " + addr.getHostAddress());
-					return addr;
-				}
-			}
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-		return null;
+	public void stopTelnetListener() {
+		Debug.msg("Stopping TelnetListener");
+		interrupt();
 	}
 }
