af745028adbbff485014565f2239517a4defcfa0
Robert Palmer <jfritz@robotniko.de>
2005-07-20 10:39:56 +0000

    Thread executed on Shutdown

---
+++ src/de/moonflower/jfritz/ShutdownThread.java
@@ -0,0 +1,35 @@
+/*
+ * Created on 20.07.2005
+ */
+package de.moonflower.jfritz;
+
+import de.moonflower.jfritz.utils.Debug;
+
+/**
+ * @author rob
+ */
+public class ShutdownThread extends Thread{
+
+	private JFritz jfritz;
+
+	public ShutdownThread (JFritz jfritz)
+	{
+		this.jfritz = jfritz;
+	}
+
+    public void run() {
+
+   	Debug.msg("Start Shutdown Thread");
+
+   	jfritz.getJframe().saveQuickDials();
+   	jfritz.getPhonebook().saveToXMLFile(JFritz.PHONEBOOK_FILE);
+	jfritz.getCallerlist().saveToXMLFile(JFritz.CALLS_FILE);
+
+	jfritz.saveProperties();
+
+	jfritz.stopCallMonitor();
+
+	Debug.msg("Shutdown Thread done");
+
+    }
+}
