07af4cfe2ff91f9521e6c0f59671b9e125da8efd
Jochen Fahrner <jfahrner@users.sourceforge.net>
2005-07-25 15:39:34 +0000

    Neu: Plattformunabhängiger Start eines Webbrowser

---
+++ src/de/moonflower/jfritz/utils/BrowserLaunch.java
@@ -0,0 +1,51 @@
+package de.moonflower.jfritz.utils;
+
+////////////////////////////////////////////////////////
+//Bare Bones Browser Launch                          //
+//Version 1.1                                        //
+//July 8, 2005                                       //
+//Supports: Mac OS X, GNU/Linux, Unix, Windows XP    //
+//Example Usage:                                     //
+// String url = "http://www.centerkey.com/";         //
+// BrowserLaunch.openURL(url);                       //
+//Public Domain Software -- Free to Use as You Like  //
+////////////////////////////////////////////////////////
+
+import java.lang.reflect.Method;
+import javax.swing.JOptionPane;
+
+public class BrowserLaunch {
+
+private static final String errMsg = "Error attempting to launch web browser";
+
+public static void openURL(String url) {
+   String osName = System.getProperty("os.name");
+   try {
+      if (osName.startsWith("Mac OS")) {
+         Class macUtils = Class.forName("com.apple.mrj.MRJFileUtils");
+         Method openURL = macUtils.getDeclaredMethod("openURL",
+            new Class[] {String.class});
+         openURL.invoke(null, new Object[] {url});
+         }
+      else if (osName.startsWith("Windows"))
+         Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+      else { //assume Unix or Linux
+         String[] browsers = {
+            "firefox", "opera", "konqueror", "mozilla", "netscape" };
+         String browser = null;
+         for (int count = 0; count < browsers.length && browser == null; count++)
+            if (Runtime.getRuntime().exec(
+                  new String[] {"which", browsers[count]}).waitFor() == 0)
+               browser = browsers[count];
+         if (browser == null)
+            throw new Exception("Could not find web browser.");
+         else
+            Runtime.getRuntime().exec(new String[] {browser, url});
+         }
+      }
+   catch (Exception e) {
+      JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
+      }
+   }
+
+}
