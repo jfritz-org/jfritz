415c4310a159083715bf9b5b123138f9b25735d9
Robert Palmer <jfritz@robotniko.de>
2005-08-10 16:45:18 +0000

    unnötige Imports entfernt

--- src/de/moonflower/jfritz/callerlist/CallerListPanel.java
+++ src/de/moonflower/jfritz/callerlist/CallerListPanel.java
@@ -16,7 +16,6 @@ import java.util.Date;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
