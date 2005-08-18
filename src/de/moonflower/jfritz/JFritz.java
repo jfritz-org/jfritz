7c9d8fdd733c1bf8979714f18b9dd98a54208e8f
Robert Palmer <jfritz@robotniko.de>
2005-08-18 14:18:20 +0000

    Sound on callin / callout

--- src/de/moonflower/jfritz/JFritz.java
+++ src/de/moonflower/jfritz/JFritz.java
@@ -228,7 +228,7 @@ public final class JFritz {
 
 	public final static String DOCUMENTATION_URL = "http://jfritz.sourceforge.net/documentation.php";
 
-	public final static String CVS_TAG = "$Id: JFritz.java,v 1.101 2005/08/18 12:45:42 robotniko Exp $";
+	public final static String CVS_TAG = "$Id: JFritz.java,v 1.102 2005/08/18 14:18:20 robotniko Exp $";
 
 	public final static String PROGRAM_AUTHOR = "Arno Willig <akw@thinkwiki.org>";
 
@@ -270,7 +270,7 @@ public final class JFritz {
 
 	private static PhoneBook phonebook;
 
-	//private static URL ringSound, callSound;
+    private static URL ringSound, callSound;
 
 	private CallMonitor callMonitor = null;
 
@@ -374,10 +374,10 @@ public final class JFritz {
 	 * Loads sounds from resources
 	 */
 	private void loadSounds() {
-		//	ringSound = getClass().getResource(
-		//			"/de/moonflower/jfritz/resources/sounds/call_in.wav");
-		//	callSound = getClass().getResource(
-		//			"/de/moonflower/jfritz/resources/sounds/call_out.wav");
+		ringSound = getClass().getResource(
+					"/de/moonflower/jfritz/resources/sounds/call_in.wav");
+		callSound = getClass().getResource(
+					"/de/moonflower/jfritz/resources/sounds/call_out.wav");
 	}
 
 	/**
@@ -396,7 +396,7 @@ public final class JFritz {
 	 * Main method for starting JFritz!
 	 *
 	 * @param args
-	 *            Program arguments (-h -v)
+	 *            Program arguments (-h -v ...)
 	 */
 	public static void main(String[] args) {
 		System.out.println(PROGRAM_NAME + " v" + PROGRAM_VERSION
@@ -596,7 +596,7 @@ public final class JFritz {
 	}
 
 	/**
-	 * Displays balloon info message TODO: Window when no systray_support
+	 * Displays balloon info message
 	 *
 	 * @param msg
 	 *            Message to be displayed
@@ -712,6 +712,7 @@ public final class JFritz {
 
 		if (JFritzUtils.parseBoolean(JFritz.getProperty("option.playSounds",
 				"true"))) {
+			playSound(ringSound);
 		}
 	}
 
@@ -728,6 +729,7 @@ public final class JFritz {
 		infoMsg("Ausgehender Telefonanruf\n " + "\nan " + calledstr + "!");
 		if (JFritzUtils.parseBoolean(JFritz.getProperty("option.playSounds",
 				"true"))) {
+			playSound(callSound);
 		}
 	}
 
