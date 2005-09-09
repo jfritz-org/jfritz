9c7de5f2d933271035d103fa9a581e7ddfafc334
Robert Palmer <jfritz@robotniko.de>
2005-09-09 12:27:44 +0000

    Erweiterte Einstellungen

--- src/de/moonflower/jfritz/dialogs/config/SyslogConfigDialog.java
+++ src/de/moonflower/jfritz/dialogs/config/SyslogConfigDialog.java
@@ -12,10 +12,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.InetAddress;
+import java.util.Enumeration;
+import java.util.Vector;
 
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -49,6 +54,10 @@ public class SyslogConfigDialog extends JDialog {
 
 	private int exitCode = 0;
 
+	private JComboBox ipAddressComboBox;
+
+	private JCheckBox syslogPassthroughCheckBox;
+
 	public static final int APPROVE_OPTION = 1;
 
 	public static final int CANCEL_OPTION = 2;
@@ -73,9 +82,12 @@ public class SyslogConfigDialog extends JDialog {
 
 	public void initDialog() {
 		setTitle("Syslog - Einstellungen");
-		setSize(250, 200);
+		setSize(270, 300);
 		drawDialog();
 		setProperties();
+	}
+
+	public void setIP(String ip) {
 	}
 
 	private void setProperties() {
@@ -83,6 +95,13 @@ public class SyslogConfigDialog extends JDialog {
 				"syslog.checkSyslog", "true")));
 		checkTelefon.setSelected(JFritzUtils.parseBoolean(JFritz.getProperty(
 				"syslog.checkTelefon", "true")));
+		ipAddressComboBox.setSelectedItem(JFritz.getProperty(
+				"option.syslogclientip", "192.168.178.21"));
+		syslogPassthroughCheckBox.setSelected(JFritzUtils.parseBoolean(JFritz
+				.getProperty("option.syslogpassthrough", "false")));
+		// Anhand von Problemen mit dem Passthrough ist diese Checkbox erst
+		// einmal deaktiviert
+		syslogPassthroughCheckBox.setEnabled(false);
 	}
 
 	private void storeProperties() {
@@ -90,6 +109,10 @@ public class SyslogConfigDialog extends JDialog {
 				.isSelected()));
 		JFritz.setProperty("syslog.checkTelefon", Boolean.toString(checkTelefon
 				.isSelected()));
+		JFritz.setProperty("option.syslogclientip", ipAddressComboBox
+				.getSelectedItem().toString());
+		JFritz.setProperty("option.syslogpassthrough", Boolean
+				.toString(syslogPassthroughCheckBox.isSelected()));
 	}
 
 	public int showSyslogConfigDialog() {
@@ -149,10 +172,14 @@ public class SyslogConfigDialog extends JDialog {
 					Telnet telnet = new Telnet(jfritz);
 					telnet.connect();
 					if (telnet.isConnected()) {
-						SyslogListener.restartTelefonOnFritzBox(telnet, jfritz);
+						if (SyslogListener.restartTelefonOnFritzBox(telnet, jfritz) == JOptionPane.YES_OPTION) {
+							JFritz.infoMsg("Telefond erfolgreich gestartet");
+							Debug.msg("Telefond restarted successfully");
+						} else {
+							JFritz.infoMsg("Telefond nicht gestartet");
+							Debug.msg("Telefond not restarted");
+						}
 						telnet.disconnect();
-						JFritz.infoMsg("Telefond erfolgreich gestartet");
-						Debug.msg("Telefond restarted successfully");
 					}
 					else {
 						JFritz.infoMsg("Fehler beim Verbinden mit Telnet");
@@ -169,8 +196,40 @@ public class SyslogConfigDialog extends JDialog {
 		c.insets.bottom = 5;
 		c.anchor = GridBagConstraints.WEST;
 
-		c.gridwidth = 1;
-		c.gridy = 0;
+		c.gridx = 0;
+		c.gridy = 1;
+		JLabel ipAddressLabel = new JLabel("Lokale IP-Adresse: ");
+		panel.add(ipAddressLabel, c);
+
+		c.gridx = 1;
+		c.gridy = 1;
+		ipAddressComboBox = new JComboBox();
+		Vector ipAddresses = new Vector();
+		ipAddresses = SyslogListener.getIP();
+		Enumeration en = ipAddresses.elements();
+		while (en.hasMoreElements()) {
+			InetAddress ad = (InetAddress) en.nextElement();
+			ipAddressComboBox.addItem(ad.toString().substring(1,
+					ad.toString().length()));
+
+		}
+
+		ipAddressComboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Debug.msg(ipAddressComboBox.getSelectedItem().toString());
+				JFritz.setProperty("option.syslogclientip", ipAddressComboBox
+						.getSelectedItem().toString());
+			}
+		});
+		panel.add(ipAddressComboBox, c);
+
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridwidth = 2;
+		syslogPassthroughCheckBox = new JCheckBox("Syslog-passthrough?");
+		panel.add(syslogPassthroughCheckBox, c);
+
+		c.gridy = 3;
 		checkSyslog = new JCheckBox("Überprüfe syslogd");
 		checkSyslog
 				.setToolTipText("Überprüft mittels Telnet, ob der Syslog-Daemon"
@@ -178,7 +237,7 @@ public class SyslogConfigDialog extends JDialog {
 						+ " und startet ihn gegebenenfalls neu.");
 		panel.add(checkSyslog, c);
 
-		c.gridy = 1;
+		c.gridy = 4;
 		checkTelefon = new JCheckBox("Überprüfe telefond");
 		checkTelefon
 		.setToolTipText("Überprüft mittels Telnet, ob der Telefon-Daemon"
@@ -186,7 +245,7 @@ public class SyslogConfigDialog extends JDialog {
 				+ " und startet ihn gegebenenfalls neu.");
 		panel.add(checkTelefon, c);
 
-		c.gridy = 2;
+		c.gridy = 5;
 		JButton restartSyslog = new JButton("Restart Syslogd on FritzBox");
 		restartSyslog.setActionCommand("restartSyslog");
 		restartSyslog.addActionListener(actionListener);
@@ -194,7 +253,7 @@ public class SyslogConfigDialog extends JDialog {
 		.setToolTipText("Startet den sylsog-Dienst auf der FritzBox neu.");
 		panel.add(restartSyslog, c);
 
-		c.gridy = 3;
+		c.gridy = 6;
 		JButton restartTelefon = new JButton("Restart Telefond on FritzBox");
 		restartTelefon.setActionCommand("restartTelefon");
 		restartTelefon.addActionListener(actionListener);
