329852a16e8605c61429120190ad7d5cb9c1f638
Robert Palmer <jfritz@robotniko.de>
2005-10-28 14:35:46 +0000

    *** empty log message ***

---
+++ src/de/moonflower/jfritz/callerlist/CommentCellEditor.java
@@ -0,0 +1,84 @@
+/*
+ *
+ * Created on 14.04.2005
+ *
+ */
+package de.moonflower.jfritz.callerlist;
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.AbstractCellEditor;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.TableCellEditor;
+
+import de.moonflower.jfritz.struct.Call;
+
+/**
+ * This deprecated class manages editing of the participant cell in the caller table.
+ *
+ * @author Arno Willig
+ *
+ */
+public class CommentCellEditor extends AbstractCellEditor implements
+		TableCellEditor {
+	private static final long serialVersionUID = 1;
+	private JTextField textField = new JTextField();
+	private Call call;
+
+	/**
+	 * @see javax.swing.table.TableCellEditor#getTableCellEditorComponent(javax.swing.JTable,
+	 *      java.lang.Object, boolean, int, int)
+	 */
+	public Component getTableCellEditorComponent(JTable table, Object value,
+			boolean isSelected, int row, int column) {
+		if (isSelected) {
+			// cell (and perhaps other cells) are selected
+		}
+		textField.setBackground(new Color(127, 255, 255));
+		// Configure the component with the specified value
+		String strval = "";
+		if (value != null)
+			strval = value.toString();
+		textField.setText(strval);
+		this.call = (Call) ((CallerList) ((CallerTable) table).getModel()).getFilteredCallVector().get(row);
+
+		// Return the configured component
+		return textField;
+	}
+
+	/**
+	 * @see javax.swing.CellEditor#getCellEditorValue()
+	 */
+	public Object getCellEditorValue() {
+		return textField.getText();
+	}
+
+	public boolean stopCellEditing() {
+		String s = (String) getCellEditorValue();
+		if (!isValid(s)) { // Should display an error message at this point
+			return false;
+		}
+		call.setComment(s);
+		return super.stopCellEditing();
+	}
+
+	public boolean isValid(String s) {
+		return true;
+	}
+
+	/**
+	 * @see javax.swing.AbstractCellEditor#fireEditingCanceled()
+	 */
+	protected void fireEditingCanceled() {
+		super.fireEditingCanceled();
+	}
+
+	/*
+	 * @see javax.swing.AbstractCellEditor#fireEditingStopped()
+	 */
+	protected void fireEditingStopped() {
+		super.fireEditingStopped();
+	}
+
+}
