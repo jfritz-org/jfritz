e79b3cb13ea46c3ba85dc4f5f5a20df46d55717a
Robert Palmer <jfritz@robotniko.de>
2005-11-06 15:37:51 +0000

    Menuepunkt "Drucken" in "Anrufliste drucken speichern" geändert

--- src/de/moonflower/jfritz/JFritzWindow.java
+++ src/de/moonflower/jfritz/JFritzWindow.java
@@ -363,7 +363,7 @@ public class JFritzWindow extends JFrame implements Runnable, ActionListener,
         item.addActionListener(this);
         jfritzMenu.add(item);
 
-        item = new JMenuItem("Drucken");
+        item = new JMenuItem("Anrufliste drucken/speichern");
         item.setActionCommand("print_callerlist");
         item.addActionListener(this);
         jfritzMenu.add(item);
