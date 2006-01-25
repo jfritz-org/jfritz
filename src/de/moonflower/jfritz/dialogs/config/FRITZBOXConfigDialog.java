c70853a9210aa0fb67716e006d4724990eab63a1
Robert Palmer <jfritz@robotniko.de>
2006-01-25 21:03:54 +0000

    Neuer ConfigDialog für neuen Anrufmonitor

---
+++ src/de/moonflower/jfritz/dialogs/config/FRITZBOXConfigDialog.java
@@ -0,0 +1,163 @@
+/*
+ * Created on 09.09.2005
+ *
+ */
+package de.moonflower.jfritz.dialogs.config;
+
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import de.moonflower.jfritz.JFritz;
+import de.moonflower.jfritz.utils.JFritzUtils;
+
+/**
+ * @author Robert Palmer
+ *
+ */
+public class FRITZBOXConfigDialog extends JDialog implements
+        CallMonitorConfigDialog {
+
+    /**
+     * This avoids compiler warnings I don't know what it's for yet
+     */
+    private static final long serialVersionUID = -8662130877265779872L;
+
+    private int exitCode = 0;
+
+    private JButton cancelButton, okButton;
+
+    public static final int APPROVE_OPTION = 1;
+
+    public static final int CANCEL_OPTION = 2;
+
+    private JCheckBox monitorIncomingCalls, monitorOutgoingCalls;
+
+    private JTextField ignoreMSN;
+
+    public FRITZBOXConfigDialog(JDialog parent, JFritz jfritz) {
+        super(parent, true);
+        if (parent != null) {
+            setLocationRelativeTo(parent);
+        }
+        // this.jfritz = jfritz;
+        initDialog();
+    }
+
+    public void initDialog() {
+        setTitle("FRITZ!Box - Einstellungen");
+        setSize(270, 240);
+        drawDialog();
+        setProperties();
+    }
+
+    private void setProperties() {
+        monitorIncomingCalls.setSelected(JFritzUtils
+                .parseBoolean(JFritz.getProperty(
+                        "option.callmonitor.monitorIncomingCalls", "true")));
+        monitorOutgoingCalls.setSelected(JFritzUtils
+                .parseBoolean(JFritz.getProperty(
+                        "option.callmonitor.monitorOutgoingCalls", "false")));
+        ignoreMSN.setText(JFritz.getProperty("option.callmonitor.ignoreMSN",""));
+    }
+
+    private void storeProperties() {
+        JFritz.setProperty("option.callmonitor.monitorIncomingCalls", Boolean
+                .toString(monitorIncomingCalls.isSelected()));
+        JFritz.setProperty("option.callmonitor.monitorOutgoingCalls", Boolean
+                .toString(monitorOutgoingCalls.isSelected()));
+        JFritz.setProperty("option.callmonitor.ignoreMSN", ignoreMSN.getText());
+    }
+
+    public int showConfigDialog() {
+        // super.show();
+        super.setVisible(true);
+        return exitCode;
+    }
+
+    private void drawDialog() {
+        KeyListener keyListener = (new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                // Cancel
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE
+                        || (e.getSource() == cancelButton && e.getKeyCode() == KeyEvent.VK_ENTER)) {
+                    exitCode = CANCEL_OPTION;
+                    setVisible(false);
+                }
+                // OK
+                if (e.getSource() == okButton
+                        && e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    storeProperties();
+                    exitCode = APPROVE_OPTION;
+                    setVisible(false);
+                }
+            }
+        });
+        ActionListener actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Object source = e.getSource();
+                if (source == okButton) {
+                    // OK
+                    exitCode = APPROVE_OPTION;
+                    storeProperties();
+                } else if (source == cancelButton) {
+                    exitCode = CANCEL_OPTION;
+                }
+                // Close Window
+                if (source == okButton || source == cancelButton) {
+                    setVisible(false);
+                }
+            }
+        };
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets.top = 5;
+        c.insets.bottom = 5;
+        c.anchor = GridBagConstraints.WEST;
+
+        c.gridwidth = 1;
+        c.gridy = 0;
+        monitorIncomingCalls = new JCheckBox("Ankommende Anrufe anzeigen");
+        panel.add(monitorIncomingCalls, c);
+        c.gridy = 1;
+        monitorOutgoingCalls = new JCheckBox("Abgehende Anrufe anzeigen");
+        panel.add(monitorOutgoingCalls, c);
+        c.gridy = 2;
+        JLabel label = new JLabel("Zu ignorierende MSNs mit ; getrennt");
+        panel.add(label, c);
+        c.gridy = 3;
+        ignoreMSN = new JTextField("", 20);
+        panel.add(ignoreMSN, c);
+
+        JPanel buttonPanel = new JPanel();
+        okButton = new JButton(JFritz.getMessage("okay"));
+        okButton.setActionCommand("ok_pressed");
+        okButton.addActionListener(actionListener);
+        okButton.addKeyListener(keyListener);
+
+        cancelButton = new JButton(JFritz.getMessage("cancel"));
+        cancelButton.setActionCommand("cancel_pressed");
+        cancelButton.addActionListener(actionListener);
+        cancelButton.addKeyListener(keyListener);
+
+        buttonPanel.add(okButton);
+        buttonPanel.add(cancelButton);
+
+        getContentPane().add(panel, BorderLayout.CENTER);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+    }
+}
