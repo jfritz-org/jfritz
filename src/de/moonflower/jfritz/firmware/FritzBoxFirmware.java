30613a360f1e3622447dac6477a2889dabf2b295
Robert Palmer <jfritz@robotniko.de>
2006-01-29 14:16:31 +0000

    Unnötiges import entfernt

--- src/de/moonflower/jfritz/firmware/FritzBoxFirmware.java
+++ src/de/moonflower/jfritz/firmware/FritzBoxFirmware.java
@@ -7,7 +7,6 @@ package de.moonflower.jfritz.firmware;
 
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.text.DecimalFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
