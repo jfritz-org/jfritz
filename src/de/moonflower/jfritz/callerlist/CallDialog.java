24e13d0078099e333f2575d49246351f3f15dbae
Bastian Schaefer <baefer@users.sourceforge.net>
2006-03-28 17:46:00 +0000

    Kostenhinweis

--- src/de/moonflower/jfritz/callerlist/CallDialog.java
+++ src/de/moonflower/jfritz/callerlist/CallDialog.java
@@ -28,6 +28,7 @@ import de.moonflower.jfritz.firmware.FritzBoxFirmware;
 import de.moonflower.jfritz.struct.PhoneNumber;
 import de.moonflower.jfritz.utils.Debug;
 import de.moonflower.jfritz.utils.Encryption;
+import de.moonflower.jfritz.utils.InfoDialog;
 import de.moonflower.jfritz.utils.JFritzUtils;
 
 /**
@@ -35,211 +36,229 @@ import de.moonflower.jfritz.utils.JFritzUtils;
  *
  */
 public class CallDialog extends JDialog implements ActionListener {
-    private static final long serialVersionUID = 1;
+	private static final long serialVersionUID = 1;
 
-    private Vector numbers;
+	private Vector numbers;
 
-    private JFritz jfritz;
+	private JFritz jfritz;
 
-    private JComboBox port;
+	private JComboBox port;
 
-    private FritzBoxFirmware firmware = null;
+	private FritzBoxFirmware firmware = null;
 
-    JButton okButton, cancelButton;
+	JButton okButton, cancelButton;
 
-    private Object cboNumber;
+	private Object cboNumber;
 
-    private PhoneNumber defaultNumber;
+	private PhoneNumber defaultNumber;
 
-    /**
-     *
-     * @param jfritz
-     *            JFritz object
-     * @param number
-     *            PhoneNumber object
-     * @throws HeadlessException
-     */
-    public CallDialog(JFritz jfritz, Vector numbers, PhoneNumber defaultNumber)
-            throws HeadlessException {
-        super();
-        this.jfritz = jfritz;
-        //this.setLocationRelativeTo(jfritz.getJframe());
-        this.setLocation(jfritz.getJframe().getX()+80, jfritz.getJframe().getY()+100);
-        this.numbers = numbers;
-       	this.defaultNumber = defaultNumber;
-        drawDialog();
-    }
+	/**
+	 *
+	 * @param jfritz
+	 *            JFritz object
+	 * @param number
+	 *            PhoneNumber object
+	 * @throws HeadlessException
+	 */
+	public CallDialog(JFritz jfritz, Vector numbers, PhoneNumber defaultNumber)
+			throws HeadlessException {
+		super();
+		this.jfritz = jfritz;
+		// this.setLocationRelativeTo(jfritz.getJframe());
+		this.setLocation(jfritz.getJframe().getX() + 80, jfritz.getJframe()
+				.getY() + 100);
+		this.numbers = numbers;
+		this.defaultNumber = defaultNumber;
+		drawDialog();
+	}
 
-    /**
-     *
-     * @param jfritz
-     *            JFritz object
-     * @param number
-     *            PhoneNumber object
-     * @throws HeadlessException
-     */
-    public CallDialog(JFritz jfritz, PhoneNumber number)
-            throws HeadlessException {
-        super();
-        this.jfritz = jfritz;
-        //this.setLocationRelativeTo(jfritz.getJframe());
-        this.setLocation(jfritz.getJframe().getX()+80, jfritz.getJframe().getY()+100);
-        Vector v = new Vector();
-        v.addElement(number);
-        this.numbers = v;
-        drawDialog();
-    }
+	/**
+	 *
+	 * @param jfritz
+	 *            JFritz object
+	 * @param number
+	 *            PhoneNumber object
+	 * @throws HeadlessException
+	 */
+	public CallDialog(JFritz jfritz, PhoneNumber number)
+			throws HeadlessException {
+		super();
+		this.jfritz = jfritz;
+		// this.setLocationRelativeTo(jfritz.getJframe());
+		this.setLocation(jfritz.getJframe().getX() + 80, jfritz.getJframe()
+				.getY() + 100);
+		Vector v = new Vector();
+		v.addElement(number);
+		this.numbers = v;
+		drawDialog();
+	}
 
-    private void drawDialog() {
-        super.dialogInit();
-        setTitle("Anrufen");
- //       this.setAlwaysOnTop(true); //erst ab Java V.5.0 möglich
-        setModal(true);
-        getContentPane().setLayout(new BorderLayout());
+	private void drawDialog() {
+		InfoDialog info = new InfoDialog(
+				jfritz,
+				"<html><body><font color=\"#FF0000\" size = \"4\"><b>ACHTUNG!</b></font><br>Die Wahlhilfe kann Telefongebühren verursachen!</body></html>");
+		info.setVisible(true);
+		info.dispose();
+		if (info.isAccepted()) {
+			super.dialogInit();
+			setTitle("Anrufen");
+			// this.setAlwaysOnTop(true); //erst ab Java V.5.0 möglich
+			setModal(true);
+			getContentPane().setLayout(new BorderLayout());
 
-        JPanel topPane = new JPanel();
-        JPanel bottomPane = new JPanel();
+			JPanel topPane = new JPanel();
+			JPanel bottomPane = new JPanel();
 
-        // Top Pane
-        topPane.setLayout(new GridBagLayout());
-        topPane.setBorder(BorderFactory.createEmptyBorder(10, 20, 5, 20));
-        GridBagConstraints c = new GridBagConstraints();
-        c.insets.top = 5;
-        c.insets.bottom = 5;
-        c.insets.left = 5;
-        c.anchor = GridBagConstraints.WEST;
+			// Top Pane
+			topPane.setLayout(new GridBagLayout());
+			topPane.setBorder(BorderFactory.createEmptyBorder(10, 20, 5, 20));
+			GridBagConstraints c = new GridBagConstraints();
+			c.insets.top = 5;
+			c.insets.bottom = 5;
+			c.insets.left = 5;
+			c.anchor = GridBagConstraints.WEST;
 
-        c.gridy = 1;
-        JLabel label = new JLabel("Nummer: ");
-        topPane.add(label, c);
-        if (this.numbers.size()==1)
-        {
-        	cboNumber = new JLabel(((PhoneNumber)numbers.elementAt(0)).getShortNumber());
-        }
-        else
-        {
-	        cboNumber = new JComboBox();
-	        for (int i=0;i<this.numbers.size();i++)
-	        {
-	        	((JComboBox) cboNumber).addItem(((PhoneNumber) numbers.elementAt(i)).getShortNumber());
-	        }
-	        ((JComboBox) cboNumber).setSelectedItem(this.defaultNumber.getShortNumber());
-        }
-        topPane.add((Component)cboNumber, c);
-        c.gridy = 2;
-        label = new JLabel("Nebenstelle: ");
-        topPane.add(label, c);
+			c.gridy = 1;
+			JLabel label = new JLabel("Nummer: ");
+			topPane.add(label, c);
+			if (this.numbers.size() == 1) {
+				cboNumber = new JLabel(((PhoneNumber) numbers.elementAt(0))
+						.getShortNumber());
+			} else {
+				cboNumber = new JComboBox();
+				for (int i = 0; i < this.numbers.size(); i++) {
+					((JComboBox) cboNumber).addItem(((PhoneNumber) numbers
+							.elementAt(i)).getShortNumber());
+				}
+				((JComboBox) cboNumber).setSelectedItem(this.defaultNumber
+						.getShortNumber());
+			}
+			topPane.add((Component) cboNumber, c);
+			c.gridy = 2;
+			label = new JLabel("Nebenstelle: ");
+			topPane.add(label, c);
 
-        boolean isdone = false;
-        int connectionFailures = 0;
-        while (!isdone) {
-            try {
-                firmware = JFritzUtils.detectBoxType(JFritz
-                        .getProperty("box.firmware"), JFritz
-                        .getProperty("box.address"), Encryption.decrypt(JFritz
-                        .getProperty("box.password")));
-                isdone = true;
-            } catch (WrongPasswordException e) {
-                jfritz.getJframe().setStatus(
-                        JFritz.getMessage("password_wrong"));
-                String password = jfritz.getJframe().showPasswordDialog(
-                        Encryption.decrypt(JFritz.getProperty("box.password",
-                                "")));
-                if (password == null) { // Dialog canceled
-                    isdone = true;
-                } else {
-                    JFritz.setProperty("box.password", Encryption
-                            .encrypt(password));
-                }
-            } catch (IOException e) {
-                // Warten, falls wir von einem Standby aufwachen,
-                // oder das Netzwerk temporär nicht erreichbar ist.
-                if (connectionFailures < 5) {
-                    Debug.msg("Waiting for FritzBox, retrying ...");
-                    connectionFailures++;
-                } else {
-                    Debug.msg("Callerlist Box not found");
-                    String box_address = jfritz.getJframe().showAddressDialog(
-                            JFritz.getProperty("box.address", "fritz.box"));
-                    if (box_address == null) { // Dialog canceled
-                        isdone = true;
-                    } else {
-                        JFritz.setProperty("box.address", box_address);
-                    }
-                }
-            }
-        }
+			boolean isdone = false;
+			int connectionFailures = 0;
+			while (!isdone) {
+				try {
+					firmware = JFritzUtils.detectBoxType(JFritz
+							.getProperty("box.firmware"), JFritz
+							.getProperty("box.address"), Encryption
+							.decrypt(JFritz.getProperty("box.password")));
+					isdone = true;
+				} catch (WrongPasswordException e) {
+					jfritz.getJframe().setStatus(
+							JFritz.getMessage("password_wrong"));
+					String password = jfritz.getJframe().showPasswordDialog(
+							Encryption.decrypt(JFritz.getProperty(
+									"box.password", "")));
+					if (password == null) { // Dialog canceled
+						isdone = true;
+					} else {
+						JFritz.setProperty("box.password", Encryption
+								.encrypt(password));
+					}
+				} catch (IOException e) {
+					// Warten, falls wir von einem Standby aufwachen,
+					// oder das Netzwerk temporär nicht erreichbar ist.
+					if (connectionFailures < 5) {
+						Debug.msg("Waiting for FritzBox, retrying ...");
+						connectionFailures++;
+					} else {
+						Debug.msg("Callerlist Box not found");
+						String box_address = jfritz.getJframe()
+								.showAddressDialog(
+										JFritz.getProperty("box.address",
+												"fritz.box"));
+						if (box_address == null) { // Dialog canceled
+							isdone = true;
+						} else {
+							JFritz.setProperty("box.address", box_address);
+						}
+					}
+				}
+			}
 
-        port = new JComboBox();
-        port.addItem("Fon 1");
-        port.addItem("Fon 2");
-        if (firmware != null) {
-            switch (firmware.getBoxType()) {
-            case FritzBoxFirmware.BOXTYPE_FRITZBOX_FON:
-                break;
-            case FritzBoxFirmware.BOXTYPE_FRITZBOX_FON_WLAN:
-                port.addItem("Analog Alle"); //ggf. kann dies auch für die anderen Boxen gelten?
-            	break;
-            case FritzBoxFirmware.BOXTYPE_FRITZBOX_ATA:
-                break;
-            case FritzBoxFirmware.BOXTYPE_FRITZBOX_5050: {
-                port.addItem("ISDN Alle");
-                port.addItem("ISDN 1");
-                port.addItem("ISDN 2");
-                port.addItem("ISDN 3");
-                port.addItem("ISDN 4");
-                port.addItem("ISDN 5");
-                port.addItem("ISDN 6");
-                port.addItem("ISDN 7");
-                port.addItem("ISDN 8");
-                port.addItem("ISDN 9");
-            }
-            case FritzBoxFirmware.BOXTYPE_FRITZBOX_7050: {
-                port.addItem("ISDN Alle");
-                port.addItem("ISDN 1");
-                port.addItem("ISDN 2");
-                port.addItem("ISDN 3");
-                port.addItem("ISDN 4");
-                port.addItem("ISDN 5");
-                port.addItem("ISDN 6");
-                port.addItem("ISDN 7");
-                port.addItem("ISDN 8");
-                port.addItem("ISDN 9");
-            }
-            }
-        }
-        topPane.add(port, c);
+			port = new JComboBox();
+			port.addItem("Fon 1");
+			port.addItem("Fon 2");
+			if (firmware != null) {
+				switch (firmware.getBoxType()) {
+					case FritzBoxFirmware.BOXTYPE_FRITZBOX_FON :
+						break;
+					case FritzBoxFirmware.BOXTYPE_FRITZBOX_FON_WLAN :
+						port.addItem("Analog Alle"); // ggf. kann dies auch
+														// für die anderen Boxen
+														// gelten?
+						break;
+					case FritzBoxFirmware.BOXTYPE_FRITZBOX_ATA :
+						break;
+					case FritzBoxFirmware.BOXTYPE_FRITZBOX_5050 : {
+						port.addItem("ISDN Alle");
+						port.addItem("ISDN 1");
+						port.addItem("ISDN 2");
+						port.addItem("ISDN 3");
+						port.addItem("ISDN 4");
+						port.addItem("ISDN 5");
+						port.addItem("ISDN 6");
+						port.addItem("ISDN 7");
+						port.addItem("ISDN 8");
+						port.addItem("ISDN 9");
+					}
+					case FritzBoxFirmware.BOXTYPE_FRITZBOX_7050 : {
+						port.addItem("ISDN Alle");
+						port.addItem("ISDN 1");
+						port.addItem("ISDN 2");
+						port.addItem("ISDN 3");
+						port.addItem("ISDN 4");
+						port.addItem("ISDN 5");
+						port.addItem("ISDN 6");
+						port.addItem("ISDN 7");
+						port.addItem("ISDN 8");
+						port.addItem("ISDN 9");
+					}
+				}
+			}
+			topPane.add(port, c);
 
-        // Bottom Pane
-        okButton = new JButton("Anrufen");
-        okButton.setActionCommand("call");
-        okButton.addActionListener(this);
+			// Bottom Pane
+			okButton = new JButton("Anrufen");
+			okButton.setActionCommand("call");
+			okButton.addActionListener(this);
 
-        cancelButton = new JButton("Abbrechen");
-        cancelButton.setActionCommand("close");
-        cancelButton.addActionListener(this);
+			cancelButton = new JButton("Abbrechen");
+			cancelButton.setActionCommand("close");
+			cancelButton.addActionListener(this);
 
-        bottomPane.add(okButton);
-        bottomPane.add(cancelButton);
+			bottomPane.add(okButton);
+			bottomPane.add(cancelButton);
 
-        getContentPane().add(topPane, BorderLayout.NORTH);
-        getContentPane().add(bottomPane, BorderLayout.SOUTH);
-        setSize(new Dimension(300, 150));
+			getContentPane().add(topPane, BorderLayout.NORTH);
+			getContentPane().add(bottomPane, BorderLayout.SOUTH);
+			setSize(new Dimension(300, 150));
 
-    }
+		}
 
-    /**
-     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-     */
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("call")) {
-        	if(cboNumber.getClass().toString().equals("class javax.swing.JLabel"))
-        		JFritzUtils.doCall(((JLabel)cboNumber).getText(), port.getSelectedItem().toString(), firmware);
-        	if(cboNumber.getClass().toString().equals("class javax.swing.JComboBox"))
-            	JFritzUtils.doCall(((JComboBox)cboNumber).getSelectedItem().toString(), port.getSelectedItem().toString(), firmware);
-             setVisible(false);
-        } else if (e.getActionCommand().equals("close")) {
-            setVisible(false);
-        }
-    }
+	}
+
+	/**
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("call")) {
+			if (cboNumber.getClass().toString().equals(
+					"class javax.swing.JLabel"))
+				JFritzUtils.doCall(((JLabel) cboNumber).getText(), port
+						.getSelectedItem().toString(), firmware);
+			if (cboNumber.getClass().toString().equals(
+					"class javax.swing.JComboBox"))
+				JFritzUtils.doCall(((JComboBox) cboNumber).getSelectedItem()
+						.toString(), port.getSelectedItem().toString(),
+						firmware);
+			setVisible(false);
+		} else if (e.getActionCommand().equals("close")) {
+			setVisible(false);
+		}
+	}
 }
---
+++ src/de/moonflower/jfritz/utils/InfoDialog.java
@@ -0,0 +1,120 @@
+package de.moonflower.jfritz.utils;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+
+import de.moonflower.jfritz.JFritz;
+
+public class InfoDialog extends JDialog implements ActionListener {
+
+	private boolean accepted = false;
+
+	private static JFritz jfritz;
+
+	private String infoText;
+
+	JButton okButton, cancelButton;
+
+	JCheckBox checkBox;
+
+	public InfoDialog(JFritz jfritz, String infoText) throws HeadlessException {
+		this.jfritz = jfritz;
+		this.infoText = infoText;
+		this.setLocation(jfritz.getJframe().getX() + 80, jfritz.getJframe()
+				.getY() + 100);
+		if (JFritz.getProperty("legalInfo.telephoneCharges", "false").equals(
+				"true")) {
+			accepted = true;
+		} else {
+			drawDialog();
+		}
+
+	}
+
+	public void drawDialog() {
+		super.dialogInit();
+		setTitle("Info");
+
+		Container c = getContentPane();
+		GridBagLayout gbl = new GridBagLayout();
+		c.setLayout(gbl);
+
+		// this.setAlwaysOnTop(true); //erst ab Java V.5.0 möglich
+		setModal(true);
+
+		// Top
+		JLabel label = new JLabel();
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setText(infoText);
+		addComponent(c, gbl, label, 0, 0, 2, 3, 1.0, 1.0, 0, 0, 0, 0);
+
+		// Center
+		okButton = new JButton("OK");
+		okButton.setActionCommand("ok");
+		okButton.addActionListener(this);
+		addComponent(c, gbl, okButton, 0, 3, 1, 1, 1.0, 0, 30, 5, 0, 0);
+
+		cancelButton = new JButton("Abbrechen");
+		cancelButton.setActionCommand("cancel");
+		cancelButton.addActionListener(this);
+		addComponent(c, gbl, cancelButton, 1, 3, 1, 1, 1.0, 0, 5, 30, 0, 0);
+
+		// Bottom
+		checkBox = new JCheckBox("Hinweis nie wieder anzeigen.");
+		checkBox.setActionCommand("call");
+		checkBox.addActionListener(this);
+		checkBox.setSelected(JFritzUtils.parseBoolean(JFritz.getProperty(
+				"legalInfo.telephoneCharges", "false")));
+		addComponent(c, gbl, checkBox, 0, 4, 2, 1, 1.0, 0, 0, 0, 5, 0);
+
+		setSize(new Dimension(300, 150));
+	}
+
+	private void addComponent(Container cont, GridBagLayout gbl, Component c,
+			int x, int y, int width, int height, double weightx,
+			double weighty, int insetsLeft, int insetsRight, int insetsTop,
+			int insetsBottom) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridwidth = width;
+		gbc.gridheight = height;
+		gbc.weightx = weightx;
+		gbc.weighty = weighty;
+		gbc.insets.left = insetsLeft;
+		gbc.insets.right = insetsRight;
+		gbc.insets.top = insetsTop;
+		gbc.insets.bottom = insetsBottom;
+		gbl.setConstraints(c, gbc);
+		cont.add(c);
+	}
+
+	public boolean isAccepted() {
+		return accepted;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("ok")) {
+			accepted = true;
+			JFritz.setProperty("legalInfo.telephoneCharges", Boolean
+					.toString(checkBox.isSelected()));
+			setVisible(false);
+		} else if (e.getActionCommand().equals("cancel")) {
+			accepted = false;
+			setVisible(false);
+		}
+	}
+
+}
