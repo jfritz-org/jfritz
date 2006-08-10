018cf9629c9b955447c05d05501621104c39d6b2
Bastian Schaefer <baefer@users.sourceforge.net>
2006-08-10 18:44:59 +0000

    *** empty log message ***

--- src/de/moonflower/jfritz/utils/ReverseLookup.java
+++ src/de/moonflower/jfritz/utils/ReverseLookup.java
@@ -26,9 +26,29 @@ import de.moonflower.jfritz.utils.ReverseLookupSwitzerland;
  */
 public class ReverseLookup {
 
-	public static final String SWITZERLAND_CODE = "+41";
+	//Sort the following COUNTRY_CODE list alphabetically
+
+	public static final String AUSTRIA_CODE = "+43";
+
+	public static final String CANADA_CODE = "+1";
+
+	public static final String FANCE_CODE = "+33";
 
 	public static final String ITALY_CODE = "+39";
+
+	public static final String GERMANY_CODE = "+49";
+
+	public static final String GREATBRITAIN_CODE = "+44";
+
+	public static final String RUSSIA_CODE = "+7";
+
+	public static final String SPAIN_CODE = "+34";
+
+	public static final String SWITZERLAND_CODE = "+41";
+
+	public static final String USA_CODE = "+1";
+
+
 
 
 	public static Person lookup(PhoneNumber number) {
@@ -49,8 +69,11 @@ public class ReverseLookup {
 				newPerson = ReverseLookupSwitzerland.lookup(number.getAreaNumber());
 			else if(number.convertToIntNumber().startsWith(ITALY_CODE))
 				newPerson = ReverseLookupItaly.lookup(number.getAreaNumber());
-			else
+			else if(number.convertToIntNumber().startsWith(GERMANY_CODE))
 				newPerson = lookupDasOertliche(number.getAreaNumber());
+			else
+				newPerson = new Person ();
+				newPerson.addNumber(number.getAreaNumber(), "home");
 		}
 		return newPerson;
 	}
