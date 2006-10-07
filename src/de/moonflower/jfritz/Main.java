c5a36356793c4d9e4ce8ba1c46d1e3a1ce48e95e
Bastian Schaefer <baefer@users.sourceforge.net>
2006-10-07 15:11:27 +0000

    *** empty log message ***

--- src/de/moonflower/jfritz/Main.java
+++ src/de/moonflower/jfritz/Main.java
@@ -537,7 +537,7 @@ public class Main {
 
 	public final static String PROGRAM_NAME = "JFritz"; //$NON-NLS-1$
 
-	public final static String PROGRAM_VERSION = "0.6.1"; //$NON-NLS-1$
+	public final static String PROGRAM_VERSION = "0.6.2"; //$NON-NLS-1$
 
 	public final static String PROGRAM_URL = "http://www.jfritz.org/"; //$NON-NLS-1$
 
