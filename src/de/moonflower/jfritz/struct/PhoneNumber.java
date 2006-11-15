7a35fed07e96716518b50d99a5cac07d1dc1abf6
Robert Palmer <jfritz@robotniko.de>
2006-11-15 20:00:58 +0000

    Bugfix: Vorwahl vor "Unknown"

--- src/de/moonflower/jfritz/struct/PhoneNumber.java
+++ src/de/moonflower/jfritz/struct/PhoneNumber.java
@@ -26,37 +26,21 @@ public class PhoneNumber implements Comparable {
 	private String callbycall = "";//$NON-NLS-1$
 
 	private String type = "";//$NON-NLS-1$
+
 	// type values : "home", "mobile", "homezone",
 	// "business", "other", "fax", "sip" };
 
-	//Please keep these in alphabetical order!!!
-	public static final String AUSTRIA_CODE="+43",
-	 					 BELGIUM_CODE="+32",
-	 					 CHINA_CODE="+86",
-	 					 CZECH_CODE="+420",
-	 					 DENMARK_CODE="+45",
-	 					 FINLAND_CODE="+358",
-	 					 FRANCE_CODE="+33",
-	 					 GERMANY_CODE="+49",
-	 					 GREATBRITAIN_CODE="+44",
-	 					 HOLLAND_CODE="+31",
-	 					 HUNGARY_CODE="+36",
-	 					 IRELAND_CODE="+353",
-	 					 ITALY_CODE = "+39",
-	 					 JAPAN_CODE="+81",
-	 					 LUXEMBOURG_CODE="+352",
-	 					 NORWAY_CODE="+47",
-	 					 POLAND_CODE="+48",
-	 					 PORTUGAL_CODE="+351",
-	 					 RUSSIA_CODE="+7",
-	 					 SLOVAKIA_CODE="+421",
-	 					 SPAIN_CODE="+34",
-	 					 SWEDEN_CODE="+46",
-	 					 SWITZERLAND_CODE = "+41",
-	 					 TURKEY_CODE="+90",
-	 					 UKRAINE_CODE="+380",
-	 					 USA_CODE="+1",
-                         INT_FREECALL="+800";
+	// Please keep these in alphabetical order!!!
+	public static final String AUSTRIA_CODE = "+43", BELGIUM_CODE = "+32",
+			CHINA_CODE = "+86", CZECH_CODE = "+420", DENMARK_CODE = "+45",
+			FINLAND_CODE = "+358", FRANCE_CODE = "+33", GERMANY_CODE = "+49",
+			GREATBRITAIN_CODE = "+44", HOLLAND_CODE = "+31",
+			HUNGARY_CODE = "+36", IRELAND_CODE = "+353", ITALY_CODE = "+39",
+			JAPAN_CODE = "+81", LUXEMBOURG_CODE = "+352", NORWAY_CODE = "+47",
+			POLAND_CODE = "+48", PORTUGAL_CODE = "+351", RUSSIA_CODE = "+7",
+			SLOVAKIA_CODE = "+421", SPAIN_CODE = "+34", SWEDEN_CODE = "+46",
+			SWITZERLAND_CODE = "+41", TURKEY_CODE = "+90",
+			UKRAINE_CODE = "+380", USA_CODE = "+1", INT_FREECALL = "+800";
 
 	static HashMap<String, String> mobileMap;
 
@@ -64,12 +48,14 @@ public class PhoneNumber implements Comparable {
 	 * @deprecated use the other constructor from now on
 	 *
 	 * Constructs a PhoneNumber with a special type
-	 * @param number Phone number
-	 * @param type Type of number
+	 * @param number
+	 *            Phone number
+	 * @param type
+	 *            Type of number
 	 */
 	public PhoneNumber(String number, String type) {
 		this.type = type;
-		//if (number.matches(numberMatcher)) this.number = number;
+		// if (number.matches(numberMatcher)) this.number = number;
 		this.number = number;
 		createMobileMap();
 		refactorNumber();
@@ -83,35 +69,46 @@ public class PhoneNumber implements Comparable {
 	public PhoneNumber(String fullNumber) {
 		this(fullNumber, "");//$NON-NLS-1$
 	}
+
 	/**
-	 * This constructor should be used if the number
-	 *  may be a quickdial and needs to be resolved!
+	 * This constructor should be used if the number may be a quickdial and
+	 * needs to be resolved!
 	 *
 	 * @author Brian Jensen
 	 *
-	 * @param fullNumber the telephone number in raw format
-	 * @param parseDialOut, a boolean value representing if a Dial out prefix needs to be parsed
+	 * @param fullNumber
+	 *            the telephone number in raw format
+	 * @param parseDialOut,
+	 *            a boolean value representing if a Dial out prefix needs to be
+	 *            parsed
 	 */
 	public PhoneNumber(String fullNumber, boolean parseDialOut) {
 		this.type = "";
-		//if (number.matches(numberMatcher)) this.number = fullNumber;
-		if (fullNumber != null && !fullNumber.equals("Unbekannt")) this.number = fullNumber;
-
-		if (parseDialOut && this.number.startsWith(Main.getProperty(
-        		"dial.prefix", " ")) ){
-        	this.number = number.substring(Main.getProperty("dial.prefix").length());
-        	Debug.msg("Parsed the dial out prefix, new number: "+this.number);
+		// if (number.matches(numberMatcher)) this.number = fullNumber;
+		if (fullNumber != null
+				&& !fullNumber.equals(Main.getMessage("unknown"))) {
+			this.number = fullNumber;
 		}
-		createMobileMap();
-		refactorNumber();
+		if (parseDialOut
+				&& this.number.startsWith(Main.getProperty("dial.prefix", " "))) {
+			this.number = number.substring(Main.getProperty("dial.prefix")
+					.length());
+			Debug.msg("Parsed the dial out prefix, new number: " + this.number);
+		}
+
+			createMobileMap();
+			refactorNumber();
 	}
 
 	/**
 	 * Sets number to this value
-	 * @param number Number to be set
+	 *
+	 * @param number
+	 *            Number to be set
 	 */
 	public void setNumber(String number) {
-		if (number.matches(numberMatcher)) this.number = number;
+		if (number.matches(numberMatcher))
+			this.number = number;
 		refactorNumber();
 	}
 
@@ -146,71 +143,73 @@ public class PhoneNumber implements Comparable {
 	 *
 	 */
 	private void removeUnnecessaryChars() {
-        number = number.replaceAll(" ","");   //$NON-NLS-1$, //$NON-NLS-2$
-	    number = number.replaceAll("\\(0","");//$NON-NLS-1$, //$NON-NLS-2$
-	    number = number.replaceAll("\\(",""); //$NON-NLS-1$, //$NON-NLS-2$
-	    number = number.replaceAll("\\)",""); //$NON-NLS-1$, //$NON-NLS-2$
+		number = number.replaceAll(" ", ""); //$NON-NLS-1$, //$NON-NLS-2$
+		number = number.replaceAll("\\(0", "");//$NON-NLS-1$, //$NON-NLS-2$
+		number = number.replaceAll("\\(", ""); //$NON-NLS-1$, //$NON-NLS-2$
+		number = number.replaceAll("\\)", ""); //$NON-NLS-1$, //$NON-NLS-2$
 	}
 
-    /**
-     * Method cuts unnecessary characters from the number, resolves quickdials, and
-     * cuts the call by from the number
-     *
-     * @author Brian Jensen
-     *
-     */
+	/**
+	 * Method cuts unnecessary characters from the number, resolves quickdials,
+	 * and cuts the call by from the number
+	 *
+	 * @author Brian Jensen
+	 *
+	 */
 	public void refactorNumber() {
-	    removeUnnecessaryChars();
-        convertQuickDial();
+		removeUnnecessaryChars();
+		convertQuickDial();
 
-        /* Part of the i18n work, don't delete
-        if(Main.getProperty("country.code","49").equals("41")){
-            callbycall = PhoneNumberSwitzerland.getCallbyCall(number);
-            number = number.substring(callbycall.length());
-            number  = convertToIntNumber();
-        }else{
-            cutCallByCall();
-            number = convertToIntNumber();
-        }*/
+		/*
+		 * Part of the i18n work, don't delete
+		 * if(Main.getProperty("country.code","49").equals("41")){ callbycall =
+		 * PhoneNumberSwitzerland.getCallbyCall(number); number =
+		 * number.substring(callbycall.length()); number = convertToIntNumber();
+		 * }else{ cutCallByCall(); number = convertToIntNumber(); }
+		 */
 
-        cutCallByCall();
+		cutCallByCall();
 		number = convertToIntNumber();
 
 	}
 
 	/**
 	 * Cuts call by call part of number
+	 *
 	 * @return Number withour call by call part
 	 */
 	private void cutCallByCall() {
 		if (number.startsWith("0100")) {//$NON-NLS-1$
 			// cut 0100yy (y = 0..9)
-			callbycall = number.substring(0,6);
+			callbycall = number.substring(0, 6);
 			number = number.substring(6);
 		} else if (number.startsWith("010")) {//$NON-NLS-1$
 			// cut 010xx (x = 1..9, y = 0..9)
-			callbycall = number.substring(0,5);
+			callbycall = number.substring(0, 5);
 			number = number.substring(5);
 		}
 	}
 
 	/**
-	 * Converts number to international number
-	 *  Internation numbers have the following format in jfritz
-	 *  (+)(Country Code)(Area Code)(Local number)
+	 * Converts number to international number Internation numbers have the
+	 * following format in jfritz (+)(Country Code)(Area Code)(Local number)
 	 *
 	 *
-	 * @TODO: This function may need to be redone, if number parsing is misbehaving
+	 * @TODO: This function may need to be redone, if number parsing is
+	 *        misbehaving
 	 *
 	 * @return Returns internationalized number
 	 */
 	public String convertToIntNumber() {
+		// do nothing if number is an "unknown" number
+		if (number.equals(Main.getMessage("unknown"))) {
+			return number;
+		}
+
 		String countryCode = Main.getProperty("country.code");//$NON-NLS-1$
 		String countryPrefix = Main.getProperty("country.prefix");//$NON-NLS-1$
 		String areaCode = Main.getProperty("area.code");//$NON-NLS-1$
 		String areaPrefix = Main.getProperty("area.prefix");//$NON-NLS-1$
-
-
 
 		if ((number.length() < 3) // A valid number??
 				|| (number.startsWith("+"))//$NON-NLS-1$
@@ -220,37 +219,42 @@ public class PhoneNumber implements Comparable {
 				|| isQuickDial() // FritzBox QuickDial
 		) {
 			return number;
-		}else	if (number.startsWith(countryPrefix))  // International call
+		} else if (number.startsWith(countryPrefix)) // International call
 			return "+" + number.substring(countryPrefix.length());//$NON-NLS-1$
 
 		else if (number.startsWith(areaPrefix))
 			return countryCode + number.substring(areaPrefix.length());//$NON-NLS-1$
 
-		/* this case should never happen!!!
-		else if (number.startsWith(countryCode.substring(1)) && number.length() > 7)
-			// International numbers without countryPrefix
-			return "+" + number;//$NON-NLS-1$
-		*/
+		/*
+		 * this case should never happen!!! else if
+		 * (number.startsWith(countryCode.substring(1)) && number.length() > 7) //
+		 * International numbers without countryPrefix return "+" +
+		 * number;//$NON-NLS-1$
+		 */
 
-		//if its not any internationl call, or a national call (in germany you can't dial
-		// a national number using the internation prefix), then its a local call
+		// if its not any internationl call, or a national call (in germany you
+		// can't dial
+		// a national number using the internation prefix), then its a local
+		// call
 		return countryCode + areaCode + number;//$NON-NLS-1$
 	}
 
 	/**
 	 * Converts number to national number, if it is a national one.
-	 * @return Returns nationalized number if country code matches, otherwise returns (unchanged) international number.
+	 *
+	 * @return Returns nationalized number if country code matches, otherwise
+	 *         returns (unchanged) international number.
 	 * @author Benjamin Schmitt
 	 */
-	public String convertToNationalNumber()
-	{
-		String countryCode = Main.getProperty("country.code","+49");//$NON-NLS-1$, //$NON-NLS-2$
-		String areaPrefix = Main.getProperty("area.prefix","0");   //$NON-NLS-1$, //$NON-NLS-2$
+	public String convertToNationalNumber() {
+		String countryCode = Main.getProperty("country.code", "+49");//$NON-NLS-1$, //$NON-NLS-2$
+		String areaPrefix = Main.getProperty("area.prefix", "0"); //$NON-NLS-1$, //$NON-NLS-2$
 
 		if (number.startsWith(countryCode)) //$NON-NLS-1$
-    		return areaPrefix + number.substring(3);
+			return areaPrefix + number.substring(3);
 
-		Debug.msg("PhoneNumber.convertToNationalNumber: this is no national number, returning unchanged (international) number"); //$NON-NLS-1$
+		Debug
+				.msg("PhoneNumber.convertToNationalNumber: this is no national number, returning unchanged (international) number"); //$NON-NLS-1$
 		return number;
 	}
 
@@ -266,7 +270,7 @@ public class PhoneNumber implements Comparable {
 	 * @return the international number
 	 */
 	public String getIntNumber() {
-		if(number.startsWith("*"))
+		if (number.startsWith("*"))
 			return JFritzUtils.convertSpecialChars(number);
 		else
 			return number;
@@ -282,8 +286,8 @@ public class PhoneNumber implements Comparable {
 
 	public String getShortNumber() {
 		String countryCode = Main.getProperty("country.code", "+49");//$NON-NLS-1$
-		String areaCode = Main.getProperty("area.code"); 		//$NON-NLS-1$
-		String areaPrefix = Main.getProperty("area.prefix");  //$NON-NLS-1$
+		String areaCode = Main.getProperty("area.code"); //$NON-NLS-1$
+		String areaPrefix = Main.getProperty("area.prefix"); //$NON-NLS-1$
 		if (number.startsWith(countryCode + areaCode)) //$NON-NLS-1$
 			return number.substring(countryCode.length() + areaCode.length());
 
@@ -308,7 +312,8 @@ public class PhoneNumber implements Comparable {
 	}
 
 	/**
-	 * @param callbycall The callbycall to set.
+	 * @param callbycall
+	 *            The callbycall to set.
 	 */
 	public void setCallByCall(String callbycall) {
 		this.callbycall = callbycall;
@@ -346,10 +351,10 @@ public class PhoneNumber implements Comparable {
 	public boolean isSIPNumber() {
 		return ((number.indexOf('@') > 0) //$NON-NLS-1$
 				// PurTel
-				|| number.startsWith("00038")  //$NON-NLS-1$
+				|| number.startsWith("00038") //$NON-NLS-1$
 				// SIPGate
-				|| number.startsWith("555")  //$NON-NLS-1$
-				// SIPGate
+				|| number.startsWith("555") //$NON-NLS-1$
+		// SIPGate
 		|| number.startsWith("777") //$NON-NLS-1$
 		);
 	}
@@ -358,11 +363,11 @@ public class PhoneNumber implements Comparable {
 	 * @return True if number is a short quickdial number
 	 */
 	public boolean isQuickDial() {
-	    if (number.startsWith("**7") || number.length() < 3) { //$NON-NLS-1$
-	        return true;
-	    } else {
-	        return false;
-	    }
+		if (number.startsWith("**7") || number.length() < 3) { //$NON-NLS-1$
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -420,13 +425,15 @@ public class PhoneNumber implements Comparable {
 	 * @return True if number is a mobile one
 	 */
 	public boolean isMobile() {
-		//		String provider = ReverseLookup.getMobileProvider(getFullNumber());
-		//		return (!provider.equals(""));
-		if(number.startsWith("+"+SWITZERLAND_CODE) && Main.getProperty("country.code", "+49").equals(SWITZERLAND_CODE))
+		// String provider = ReverseLookup.getMobileProvider(getFullNumber());
+		// return (!provider.equals(""));
+		if (number.startsWith("+" + SWITZERLAND_CODE)
+				&& Main.getProperty("country.code", "+49").equals(
+						SWITZERLAND_CODE))
 			return PhoneNumberSwitzerland.isMobile(getAreaNumber());
-		else{
+		else {
 			boolean ret = number.length() > 6
-				&& mobileMap.containsKey(number.substring(0, 6));
+					&& mobileMap.containsKey(number.substring(0, 6));
 			if (ret && getType().equals("")) //$NON-NLS-1$
 				type = "mobile"; //$NON-NLS-1$
 			return ret;
@@ -474,56 +481,58 @@ public class PhoneNumber implements Comparable {
 	}
 
 	/**
-	 * This function resolves incoming Quickdials to their appropriate
-	 * full number, if one is not found the Quickdial is left unchanged
+	 * This function resolves incoming Quickdials to their appropriate full
+	 * number, if one is not found the Quickdial is left unchanged
 	 *
 	 * @author Brian Jensen
 	 *
-   	 */
-	public void convertQuickDial(){
+	 */
+	public void convertQuickDial() {
 
 		if (number.startsWith("**7")) //$NON-NLS-1$
-        	// QuickDial
-        {
-            Debug.msg("Quickdial: " + number //$NON-NLS-1$
-                    + ", searching for the full number"); //$NON-NLS-1$
+		// QuickDial
+		{
+			Debug.msg("Quickdial: " + number //$NON-NLS-1$
+					+ ", searching for the full number"); //$NON-NLS-1$
 
-          	// replace QuickDial with
-            // QuickDial-Entry
-            String quickDialNumber = number.substring(3, 5);
-            Debug.msg("Quickdail number: "+quickDialNumber);
+			// replace QuickDial with
+			// QuickDial-Entry
+			String quickDialNumber = number.substring(3, 5);
+			Debug.msg("Quickdail number: " + quickDialNumber);
 
-            if (JFritz.getJframe().getQuickDialPanel().getDataModel()
-                    .getQuickDials().size() == 0) {
+			if (JFritz.getJframe().getQuickDialPanel().getDataModel()
+					.getQuickDials().size() == 0) {
 
-            	// get QuickDials from FritzBox
-            	Debug.msg("No Quickdials present in JFritz, retrieving the list from the box");
-            	JFritz.getJframe().getQuickDialPanel().getDataModel()
-            	.getQuickDialDataFromFritzBox();
-            }
-            Enumeration en = JFritz.getJframe().getQuickDialPanel()
-            	.getDataModel().getQuickDials().elements();
-            while (en.hasMoreElements()) {
-            	QuickDial quickDial = (QuickDial) en.nextElement();
-            	if (quickDialNumber.equals(quickDial.getQuickdial())) {
-            		number = quickDial.getNumber();
-            		Debug.msg("Quickdial resolved. Number: " //$NON-NLS-1$
-            				+ number);
-            	}
-            }
+				// get QuickDials from FritzBox
+				Debug
+						.msg("No Quickdials present in JFritz, retrieving the list from the box");
+				JFritz.getJframe().getQuickDialPanel().getDataModel()
+						.getQuickDialDataFromFritzBox();
+			}
+			Enumeration en = JFritz.getJframe().getQuickDialPanel()
+					.getDataModel().getQuickDials().elements();
+			while (en.hasMoreElements()) {
+				QuickDial quickDial = (QuickDial) en.nextElement();
+				if (quickDialNumber.equals(quickDial.getQuickdial())) {
+					number = quickDial.getNumber();
+					Debug.msg("Quickdial resolved. Number: " //$NON-NLS-1$
+							+ number);
+				}
+			}
 
-            if(number.startsWith("**7"))
-            	Debug.msg("No quickdial found. Refresh your quickdial list"); //$NON-NLS-1$
+			if (number.startsWith("**7"))
+				Debug.msg("No quickdial found. Refresh your quickdial list"); //$NON-NLS-1$
 
-        }
+		}
 	}
 
 	public boolean equals(Object number) {
 		if (!(number instanceof PhoneNumber)) {
 			return false;
 		}
-		if ( this.getIntNumber().equals(((PhoneNumber)number).getIntNumber()))
+		if (this.getIntNumber().equals(((PhoneNumber) number).getIntNumber()))
 			return true;
-		else return false;
+		else
+			return false;
 	}
 }
