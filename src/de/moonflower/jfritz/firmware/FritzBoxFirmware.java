a7624899e4048945cfb3a91d5caccf3fa4b0f214
Brian Jensen <capncrunch@users.sourceforge.net>
2007-04-14 17:20:14 +0000

    fixed compile bug

--- src/de/moonflower/jfritz/firmware/FritzBoxFirmware.java
+++ src/de/moonflower/jfritz/firmware/FritzBoxFirmware.java
@@ -44,6 +44,8 @@ public class FritzBoxFirmware {
 
     public final static byte BOXTYPE_FRITZBOX_7170 = 29;
 
+    public final static byte BOXTYPE_SPEEDPORT_W900V = 34;
+
     public final static byte ACCESS_METHOD_POST_0342 = 0;
 
 	public final static byte ACCESS_METHOD_ENGLISH = 1;
