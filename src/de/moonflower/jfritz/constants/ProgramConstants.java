52e52ab3930c7ad4df759f59cee5c0627eb78c6c
Robert Palmer <jfritz@robotniko.de>
2012-03-18 00:15:26 +0000

    Set version to 0.7.4.1.30

--- src/de/moonflower/jfritz/constants/ProgramConstants.java
+++ src/de/moonflower/jfritz/constants/ProgramConstants.java
@@ -6,7 +6,7 @@ public class ProgramConstants {
 	// when changing this, don't forget to check the resource bundles!!
 	public final static String PROGRAM_NAME = "JFritz"; //$NON-NLS-1$
 
-	public final static String PROGRAM_VERSION = "0.7.4.1.29"; //$NON-NLS-1$
+	public final static String PROGRAM_VERSION = "0.7.4.1.30"; //$NON-NLS-1$
 
 	public final static String REVISION = "241:251M";
 
