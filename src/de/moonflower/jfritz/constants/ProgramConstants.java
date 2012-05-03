1e8076d999ea6785a0578488f9d271c12aacd995
Robert Palmer <jfritz@robotniko.de>
2012-05-03 18:54:56 +0000

    ProgramConstants aktualisiert

--- src/de/moonflower/jfritz/constants/ProgramConstants.java
+++ src/de/moonflower/jfritz/constants/ProgramConstants.java
@@ -8,7 +8,7 @@ public class ProgramConstants {
 
 	public final static String PROGRAM_VERSION = "0.7.4.1.30"; //$NON-NLS-1$
 
-	public final static String REVISION = "259:260";
+	public final static String REVISION = "259:261";
 
-	public final static String BUILD_DATE = "03-05-2012 20:52:49";
+	public final static String BUILD_DATE = "03-05-2012 20:54:27";
 }
