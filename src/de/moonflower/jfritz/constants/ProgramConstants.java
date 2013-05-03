4a29267865f210e0dd6d0069b0f7d6381cd7492b
Robert Palmer <Robert.Palmer@robotniko.de>
2013-05-03 10:31:43 +0200

    Set version to 0.7.4.2 and updated changelog

--- src/de/moonflower/jfritz/constants/ProgramConstants.java
+++ src/de/moonflower/jfritz/constants/ProgramConstants.java
@@ -1,14 +1,12 @@
 package de.moonflower.jfritz.constants;
 
 public class ProgramConstants {
-	public final static String CVS_TAG = "$Id: ProgramConstants.java 131 2010-11-05 18:36:24Z robotniko $"; //$NON-NLS-1$
-
 	// when changing this, don't forget to check the resource bundles!!
 	public final static String PROGRAM_NAME = "JFritz"; //$NON-NLS-1$
 
-	public final static String PROGRAM_VERSION = "0.7.5"; //$NON-NLS-1$
+	public final static String PROGRAM_VERSION = "0.7.4.2"; //$NON-NLS-1$
 
-	public final static String REVISION = "274M";
+	public final static String REVISION = "";
 
-	public final static String BUILD_DATE = "13-01-2013 16:34:38";
+	public final static String BUILD_DATE = "03-05-2013 10:31:00";
 }
