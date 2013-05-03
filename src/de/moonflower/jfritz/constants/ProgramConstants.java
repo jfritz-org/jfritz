f0c4187d2c70dc2479bff69ba24428d9c375ea51
Robert Palmer <Robert.Palmer@robotniko.de>
2013-05-03 11:14:48 +0200

    Set Revision to 1

--- src/de/moonflower/jfritz/constants/ProgramConstants.java
+++ src/de/moonflower/jfritz/constants/ProgramConstants.java
@@ -6,7 +6,7 @@ public class ProgramConstants {
 
 	public final static String PROGRAM_VERSION = "0.7.4.2"; //$NON-NLS-1$
 
-	public final static String REVISION = "";
+	public final static String REVISION = "1";
 
 	public final static String BUILD_DATE = "03-05-2013 10:31:00";
 }
