53a5a0f95e1e79e7a97af7e6f7d23308311dd08c
Robert Palmer <Robert.Palmer@robotniko.de>
2013-11-24 16:10:21 +0100

    Updated revision to 18

--- src/de/moonflower/jfritz/constants/ProgramConstants.java
+++ src/de/moonflower/jfritz/constants/ProgramConstants.java
@@ -6,7 +6,7 @@ public class ProgramConstants {
 
 	public final static String PROGRAM_VERSION = "0.7.4.2"; //$NON-NLS-1$
 
-	public final static String REVISION = "17"; //$NON-NLS-1$
+	public final static String REVISION = "18"; //$NON-NLS-1$
 
 	public final static String BUILD_DATE = "24-11-2013 15:59:25";
 }
