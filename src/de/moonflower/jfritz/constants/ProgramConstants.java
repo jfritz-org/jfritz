254c88771a89c9bd0d59772e317eed4ef0f640ab
Robert Palmer <Robert.Palmer@robotniko.de>
2013-12-23 15:04:07 +0100

    Weihnachtsversion

--- src/de/moonflower/jfritz/constants/ProgramConstants.java
+++ src/de/moonflower/jfritz/constants/ProgramConstants.java
@@ -4,9 +4,9 @@ public class ProgramConstants {
 	// when changing this, don't forget to check the resource bundles!!
 	public final static String PROGRAM_NAME = "JFritz"; //$NON-NLS-1$
 
-	public final static String PROGRAM_VERSION = "0.7.4.2"; //$NON-NLS-1$
+	public final static String PROGRAM_VERSION = "0.7.5"; //$NON-NLS-1$
 
-	public final static String REVISION = "22"; //$NON-NLS-1$
+	public final static String REVISION = "1"; //$NON-NLS-1$
 
 	public final static String BUILD_DATE = "23-12-2013 14:36:40";
 }
